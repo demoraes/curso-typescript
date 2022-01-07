81cb4848627dd9676d2001a61e607d2cfa1cbb3b
Gabriel Moraes <gabriel.moraes@iclubs.com.br>
2022-01-07 00:20:40 -0400

    Desafio Tipo Objetos

--- introducao/tipos.js
+++ introducao/tipos.js
@@ -1,14 +1,36 @@
 "use strict";
-let contaBancaria = {
-    saldo: 3456,
-    depositar(valor) {
-        this.saldo += valor;
+let funcionarios = {
+    supervisores: ['gabriel', 'marcelo'],
+    baterPonto(hora) {
+        if (hora <= 8) {
+            return 'Ponto normal';
+        }
+        else {
+            return 'Fora do horário';
+        }
     }
 };
-let correntista = {
-    nome: 'Ana Silva',
-    contaBancaria: contaBancaria,
-    contatos: ['34567890', '98765432']
-};
-correntista.contaBancaria.depositar(3000);
-console.log(correntista);
+console.log(funcionarios.supervisores);
+console.log(funcionarios.baterPonto(10));
+// type IcontaBancaria = {
+//     saldo: number,
+//     depositar: (valor:number) => void
+// }
+// let contaBancaria: IcontaBancaria = {
+//     saldo: 3456,
+//     depositar(valor) {
+//         this.saldo += valor
+//     }
+// }
+// type Icorrentista = {
+//     nome: string,
+//     contaBancaria: IcontaBancaria
+//     contatos: string[]
+// }
+// let correntista: Icorrentista = {
+//     nome: 'Ana Silva',
+//     contaBancaria: contaBancaria,
+//     contatos: ['34567890', '98765432']
+// }
+// correntista.contaBancaria.depositar(3000)
+// console.log(correntista)
--- introducao/tipos.ts
+++ introducao/tipos.ts
@@ -1,27 +1,51 @@
-type IcontaBancaria = {
-    saldo: number,
-    depositar: (valor:number) => void
-}
-
-let contaBancaria: IcontaBancaria = {
-    saldo: 3456,
-    depositar(valor) {
-        this.saldo += valor
+let funcionarios: {
+    supervisores: string[],
+    baterPonto: (hora: number) => string 
+} = {
+    supervisores: ['gabriel','marcelo'],
+    baterPonto(hora: number): string {
+        if(hora <= 8) {
+            return 'Ponto normal';
+        } else {
+            return 'Fora do horário';
+        }
     }
 }
 
-type Icorrentista = {
-    nome: string,
-    contaBancaria: IcontaBancaria
-    contatos: string[]
-}
+console.log(funcionarios.supervisores);
+console.log(funcionarios.baterPonto(10));
 
 
-let correntista: Icorrentista = {
-    nome: 'Ana Silva',
-    contaBancaria: contaBancaria,
-    contatos: ['34567890', '98765432']
-}
 
-correntista.contaBancaria.depositar(3000)
-console.log(correntista)
+
+
+
+
+
+// type IcontaBancaria = {
+//     saldo: number,
+//     depositar: (valor:number) => void
+// }
+
+// let contaBancaria: IcontaBancaria = {
+//     saldo: 3456,
+//     depositar(valor) {
+//         this.saldo += valor
+//     }
+// }
+
+// type Icorrentista = {
+//     nome: string,
+//     contaBancaria: IcontaBancaria
+//     contatos: string[]
+// }
+
+
+// let correntista: Icorrentista = {
+//     nome: 'Ana Silva',
+//     contaBancaria: contaBancaria,
+//     contatos: ['34567890', '98765432']
+// }
+
+// correntista.contaBancaria.depositar(3000)
+// console.log(correntista)
